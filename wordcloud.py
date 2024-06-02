645636409c4626b0a1f125e47e0f1af304ce0b2e
carlokohan <carlohusmillo@gmail.com>
2024-06-03 02:32:29 +0800

    working classifying

--- wordcloud.py
+++ wordcloud.py
@@ -1,11 +1,20 @@
-def find_max(wordlist):
-    x = 1
+import operator
 
 def generate_wordcloud(wordlist):
-    huge = 1
+    wordcloud = {}
+    huge_word = max(wordlist, key=wordlist.get)
+    wordcloud[huge_word] = 'Huge'
+    huge_count = wordlist[huge_word]
 
+    for key in list(wordlist.keys()): 
+        if wordlist[key] >= (0.6 * huge_count):
+            wordcloud[key] = 'Big'
+        elif wordlist[key] >= (0.3 * huge_count):
+            wordcloud[key] = 'Normal'
+        elif wordlist[key] > 1:
+            wordcloud[key] = 'Small'
 
-
+    return wordcloud
 
 text = open("files/words.txt", "r") 
 
@@ -21,8 +30,9 @@ for line in text:
     else: 
         wordlist[word] = 1
 
-for key in list(wordlist.keys()): 
-    print(key, ":", wordlist[key])
+wordcloud = generate_wordcloud(wordlist)
+for key in list(wordcloud.keys()): 
+    print(key, ":", wordcloud[key])
 
 
 
